0ebb9fb128d3bf5bdba2cbc8609e32f2e69c9b8d
Chacha  Mbone <mbonemarycharity@gmail.com>
2026-03-15 14:37:56 +0300

    ContactPage: Full width form and removed comment section

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -111,7 +111,7 @@ const ContactPage = () => {
         className="h-auto py-10 flex items-center justify-center px-[5%] md:px-0"
         id="Contact"
       >
-        <div className="container px-[1%] grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-[45%_55%] 2xl:grid-cols-[35%_65%] gap-12">
+        <div className="container max-w-4xl mx-auto">
           <div
             data-aos="fade-right"
             data-aos-duration="1200"
